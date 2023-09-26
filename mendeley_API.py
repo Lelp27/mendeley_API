a4e2a88f7586d567eef8e59bf6e6a119705e949c
kun_laptop <tjdrns227@gmail.com>
2023-09-27 00:02:49 +0900

    download

--- mendeley_API.py
+++ mendeley_API.py
@@ -8,8 +8,17 @@ with open('./data/db.json') as f:
 
 def get_documents_from_folder(session, folder_name):
     documents = session.get(url=f'https://api.mendeley.com/folders/{folders[folder_name]}/documents').json()
-    
+    return documents
 
+def download_file(session, file_id, file_path="tmp.pdf"):
+    rsp = session.get(f"https://api.mendeley.com/files/{file_id}", stream=True)
+    with open(file_path, 'wb') as f:
+        for block in rsp.iter_content(1024):
+            if not block:
+                break
+            f.write(block)
+    
+    return file_path
 
 
 REDIRECT_URI = 'http://localhost:5000/oauth'
@@ -25,12 +34,13 @@ session = auth.authenticate('http://localhost:5000/oauth?code=yAToCMn9bfOHeDMqT4
 folders = {}
 for i in session.get(url='https://api.mendeley.com/folders').json():
     folders[i['name']] = i['id']
-documents = session.get(url=f'https://api.mendeley.com/folders/{folders["읽을 것"]}/documents').json()
+documents = get_documents_from_folder(session, "읽을 것")
 
 files = session.get(url='https://api.mendeley.com/files').json()
 files_id = {}
 for i in files:
     files_id[i["document_id"]] = files_id[i["id"]]
 
-for i in documents:
-    files_id[documents["id"]]
+for docs in documents:
+    file_id = files_id[docs["id"]]
+    file_path = download_file(session, file_id)
