e3cfd046d4506cf38e5518b40bb07e1882643dfc
Lelp27 <sanekun@kaist.ac.kr>
2023-09-27 18:06:56 +0900

    lab

--- Main.py
+++ Main.py
@@ -4,6 +4,24 @@ from streamlit_cookies_manager import EncryptedCookieManager
 from mendeley import Mendeley
 import time
 
+def get_mendeley_session(ID, SECRET, AUTH_URI, STATE):
+    mendeley = Mendeley(ID, SECRET, redirect_uri=AUTH_URI)
+    auth = mendeley.start_authorization_code_flow(state=STATE)
+    session = auth.authenticate(
+    f'{AUTH_URI}?code={st.session_state["token"]["code"][0]}&state={st.session_state["token"]["state"][0]}'
+    )
+    
+    return session
+
+def download_file(session, file_id, file_path="tmp.pdf"):
+    rsp = session.get(f"https://api.mendeley.com/files/{file_id}", stream=True)
+    with open(file_path, 'wb') as f:
+        for block in rsp.iter_content(1024):
+            if not block:
+                break
+            f.write(block)
+    
+    return file_path
 
 # Header
 cookies = EncryptedCookieManager(password='insaneSBL', expiration_seconds=60)
@@ -11,12 +29,13 @@ if not cookies.ready():
     st.stop()
 
 REDIRECT_URI = 'http://localhost:5000/oauth'
-
+MENDELEY_URI = 'https://api.mendeley.com'
 # Main
 st.sidebar.markdown(
     """
-    Write summary to mendeley annotaion with AI.
-    Author: sanekun@kaist.ac.kr
+    Summarise scientific paper with AI.     
+    Author: sanekun@kaist.ac.kr  
+    
     <a href = "https://github.com/Lelp27/mendeley_API"><img alt="GitHub" src ="https://img.shields.io/badge/GitHub-181717.svg?&style=for-the-badge&logo=GitHub&logoColor=white"/>
     """, unsafe_allow_html=True)
 st.markdown("# Mendeley API")
@@ -42,8 +61,8 @@ if not st.experimental_get_query_params():
             cookies['clientSECRET'] = SECRET
             cookies['auth_state'] = auth.state
             cookies.save()
-        
-            st.markdown(f'<a href="{login_url}" target="_self"> Authroize </a>', unsafe_allow_html=True)
+            
+            st.link_button("Authorization with Mendeley", url=login_url)
             expirate_time = 60
             progress_text = "Authorization will only work in {}s"
             author_progress = st.progress(expirate_time, progress_text.format(expirate_time))
@@ -57,11 +76,23 @@ else: # After authorize
     st.session_state['clientSECRET'] = cookies['clientSECRET']
     st.session_state['auth_state'] = cookies['auth_state']
 
-    mendeley = Mendeley(st.session_state['clientID'], st.session_state['clientSECRET'], redirect_uri=REDIRECT_URI)
-    auth = mendeley.start_authorization_code_flow(state=st.session_state['auth_state'])
-    test = f'{REDIRECT_URI}/?code={st.session_state["token"]["code"][0]}&state={st.session_state["token"]["state"][0]}'
+    session = get_mendeley_session(ID=st.session_state['clientID'],
+                                   SECRET=st.session_state['clientSECRET'],
+                                   AUTH_URI=REDIRECT_URI,
+                                   STATE=st.session_state['auth_state'])
+    if session:
+        st.success("Success get token")
 
-    session = auth.authenticate(
-    f'{REDIRECT_URI}?code={st.session_state["token"]["code"][0]}&state={st.session_state["token"]["state"][0]}'
-    )
-    st.success("Success get token")
+    collections = {i["name"]:i["id"] for i in session.get(url=f'{MENDELEY_URI}/folders').json()}
+    collection = st.selectbox(label='collections',
+                 options=collections.keys(),
+                 help="Choose collections to summrise.")
+
+    documents = session.get(url=f'{MENDELEY_URI}/folders/{collections[collection]}/documents').json()
+    documents = {i["id"]:i["name"] for i in documents}
+    
+    files = session.get(url='{MENDELEY_URI}/files').json()
+    files = {i["document_id"]:i["id"] for i in files}
+    
+    for doc_id in documents.keys():
+        download_file(session, files[doc_id])
--- mendeley_API.py
+++ mendeley_API.py
@@ -2,7 +2,6 @@ import json
 from mendeley import Mendeley
 import inspect
 
-
 with open('./data/db.json') as f:
     config = json.load(f)
 
@@ -29,7 +28,9 @@ state = auth.state
 login_url=auth.get_login_url()
 login_url
 
-session = auth.authenticate('http://localhost:5000/oauth?code=yAToCMn9bfOHeDMqT4qOTUF7i1I&state=JQPSDVX4YJA537FJXQV16UTYFIZ90Z')
+session = auth.authenticate('http://localhost:5000/oauth/?code=xVbDhTyeLARag6xK6R0j_JFU5ZU&state=PIEOJ3LAAHO3QY4MLFYW3CKU076EZT')
+
+#==================================
 
 folders = {}
 for i in session.get(url='https://api.mendeley.com/folders').json():
@@ -39,8 +40,13 @@ documents = get_documents_from_folder(session, "읽을 것")
 files = session.get(url='https://api.mendeley.com/files').json()
 files_id = {}
 for i in files:
-    files_id[i["document_id"]] = files_id[i["id"]]
+    files_id[i["document_id"]] = i["id"]
 
 for docs in documents:
     file_id = files_id[docs["id"]]
     file_path = download_file(session, file_id)
+
+documents[0]["id"]
+def get_documents_from_folder(session, folder_name):
+    documents = session.get(url=f'https://api.mendeley.com/folders/{folders[folder_name]}/documents').json()
+    return documents
